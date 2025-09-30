e504e8a2e1469270f90e38555857e8522a7e1a2e
Parsa <p.mohtashami.k@gmail.com>
2025-09-29 23:33:43 -0700

    feat {integrate tailwindcss/postcss}

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,13 +8,13 @@ exports.login = async function (req, res) {
         const candidate = await user.findOne({ username });
 
         if (!candidate || !await candidate.comparePassword(password)) {
-            return res.render("layout", { fileName: "login", error: "Invalid Credentials!" });
+            return res.render("layout", { title: "Login", fileName: "login", error: "Invalid Credentials!" });
         }
 
         auth.initializeCookie(candidate, res);
         return res.redirect("/notes");
     } catch (err) {
-        return res.render("layout", { fileName: "login", error: `Internal error ${err}` });
+        return res.render("layout", { title: "Login", fileName: "login", error: `Internal error ${err}` });
     }
 }
 
@@ -23,16 +23,16 @@ exports.createUser = async function (req, res) {
         const { username, password, confirmPassword } = req.body;
 
         if (await user.findOne({ username }))
-            return res.render("layout", { fileName: "signUp", message: "Username exists!" });
+            return res.render("layout", { title: "Sign Up", fileName: "signUp", error: "Username exists!" });
         if (password !== confirmPassword)
-            return res.render("layout", { fileName: "signUp", message: "Passwords do not match." });
+            return res.render("layout", { title: "Sign Up", fileName: "signUp", error: "Passwords do not match." });
 
         const newUser = await user.create({ username, password });
 
         auth.initializeCookie(newUser, res);
         return res.redirect("/notes")
     } catch (err) {
-        return res.render("layout", { fileName: "signUp", error: `Internal error ${err}` });
+        return res.render("layout", { title: "Sign Up", fileName: "signUp", error: `Internal error ${err}` });
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 app.use(auth.setup);
 
